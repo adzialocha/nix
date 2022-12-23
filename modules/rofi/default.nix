0e3adc3b501a5dd7c7e14fc907bf0c237ba2e34d
Andreas Dzialocha <x12@adz.garden>
2022-12-23 23:41:34 +0100

    Trying to figure out why rofi theme is not accepted

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -5,10 +5,15 @@ in {
   options.modules.rofi.enable = lib.mkEnableOption "rofi";
 
   config = lib.mkIf cfg.enable {
-    programs = {
-      rofi = {
-        enable = true;
-	theme = ./config.rasi;
+    programs.rofi = {
+      enable = true;
+      cycle = true;
+      terminal = "${lib.getExe pkgs.alacritty}";
+      font = "IBM Plex Mono 11";
+      theme = ./theme.rasi;
+      extraConfig = {
+	modi = "combi";
+        combi-modi = "window,run,ssh";
       };
     };
   };
