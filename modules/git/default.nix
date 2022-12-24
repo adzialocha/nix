654835aa2c0a2aa578809f3fc655c5e3a28e0baa
Andreas Dzialocha <x12@adz.garden>
2022-12-25 00:56:12 +0100

    Adjust git configuration and add global ignore file

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -5,18 +5,44 @@ in {
   options.modules.git.enable = lib.mkEnableOption "git";
 
   config = lib.mkIf cfg.enable {
+    home.file.".gitignore-global".source =
+      config.lib.file.mkOutOfStoreSymlink ./.gitignore-global;
+
     programs.git = {
       enable = true;
+
       userName = "Andreas Dzialocha";
       userEmail = "x12@adz.garden";
+
       extraConfig = {
-        core.autocrlf = "input";
-        init.defaultBranch = "main";
-        apply.whitespace = "fix";
-        push.default = "simple";
-        pull.rebase = true;
-        merge.log = true;
-        log.abbrevcommit = true;
+        core = {
+          autocrlf = "input";
+          whitespace = "fix,space-before-tab,tab-in-indent,trailing-space";
+          excludesfile = "$HOME/.gitignore-global";
+        };
+        init = { defaultBranch = "main"; };
+        apply = { whitespace = "fix"; };
+        color = { ui = "auto"; };
+        "color \"branch\"" = {
+          current = "yellow reverse";
+          local = "yellow";
+          remote = "green";
+        };
+        "color \"diff\"" = {
+          meta = "magenta";
+          frag = "magenta bold";
+          old = "red";
+          new = "green";
+        };
+        "color \"status\"" = {
+          added = "yellow";
+          changed = "green";
+          untracked = "red";
+        };
+        push = { default = "simple"; };
+        pull = { rebase = true; };
+        merge = { log = true; };
+        log = { abbrevcommit = true; };
       };
     };
   };
