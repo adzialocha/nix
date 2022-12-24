27598e13da076fa394876796bd2b28f6bc64cb9e
Andreas Dzialocha <x12@adz.garden>
2022-12-24 01:39:08 +0100

    Make x11 a module

--- modules/x11/default.nix
+++ modules/x11/default.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  home.file.".Xresources".source = ./Xresources;
+let cfg = config.modules.x11;
+in {
+  options.modules.x11.enable = lib.mkEnableOption "x11";
+
+  config = lib.mkIf cfg.enable {
+    home.file.".Xresources".source = ./Xresources;
+  };
 }
