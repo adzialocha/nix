a157b77929fc9d4add95240517218ef3e1691690
Andreas Dzialocha <x12@adz.garden>
2022-12-28 14:53:06 +0100

    Remove this again

--- modules/nodejs/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-let cfg = config.modules.nodejs;
-in {
-  options.modules.nodejs.enable = lib.mkEnableOption "nodejs";
-
-  config =
-    lib.mkIf cfg.enable { home = { packages = with pkgs; [ nodejs ]; }; };
-}
