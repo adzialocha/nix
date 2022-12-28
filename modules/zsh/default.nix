91a02848907c84587931927aab247d92abc16e17
Andreas Dzialocha <x12@adz.garden>
2022-12-28 23:39:23 +0100

    Move all sorts of cached files in better places

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -11,10 +11,6 @@ in {
     programs.zsh = {
       enable = true;
 
-      history = {
-        path = "${config.xdg.dataHome}/zsh/history";
-      };
-
       oh-my-zsh = {
         enable = true;
         plugins = [ "cp" "git" ];
@@ -23,12 +19,30 @@ in {
       };
 
       shellGlobalAliases = {
+        vim = "nvim";
+        Ag = "ag";
+
         l = "ls -lah --group-directories-first";
         la = "ls -lAhv --group-directories-first";
         ll = "ls -lh --group-directories-first";
-        vim = "nvim";
-        Ag = "ag";
+
+        # Move `wget` HSTS file out of home directory
+        wget = "wget --hsts-file ${config.xdg.dataHome}/.wget-hsts";
       };
+
+      # Move zsh history out of home directory
+      history = { path = "${config.xdg.dataHome}/zsh/.zsh-history"; };
+
+      # Move .zcomp files out of home directory
+      initExtraFirst = ''
+        export ZDOTDIR="${config.xdg.dataHome}/zsh"
+      '';
+
+      # Move `less` history and node repl files out of home directory
+      initExtra = ''
+        export LESSHISTFILE="${config.xdg.dataHome}/.lesshst"
+        export NODE_REPL_HISTORY="${config.xdg.dataHome}/.node_repl_history"
+      '';
     };
   };
 }
