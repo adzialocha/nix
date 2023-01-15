067087139ae2f53f44c9a7c0a20d3142f587f732
Andreas Dzialocha <x12@adz.garden>
2023-01-15 13:10:27 +0100

    Both hosts have the same home manager config

--- hosts/adz-x230-laptop/home.nix
+++ hosts/adz-x230-laptop/home.nix
@@ -1,72 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ../../modules ];
-
-  xsession.enable = true;
-
-  fonts.fontconfig.enable = true;
-
-  home = {
-    username = "adz";
-    homeDirectory = "/home/adz";
-
-    sessionVariables = rec {
-      EDITOR = "nvim";
-      VISUAL = EDITOR;
-    };
-
-    packages = with pkgs; [
-      # Desktop applications
-      firefox
-      signal-desktop
-      tdesktop
-      thunderbird
-
-      # Useful tools and system libraries
-      cacert
-      curl
-      dnsutils
-      fd
-      file
-      gcc
-      htop
-      jq
-      man-pages
-      nixfmt
-      openssl
-      scrot
-      silver-searcher
-      tree
-      unzip
-      wget
-      xclip
-
-      # Fonts
-      ibm-plex
-      noto-fonts
-      noto-fonts-emoji
-      roboto-mono
-      source-code-pro
-    ];
-
-    stateVersion = "22.11";
-  };
-
-  programs = { home-manager.enable = true; };
-
-  modules = {
-    alacritty.enable = true;
-    direnv.enable = true;
-    fzf.enable = true;
-    git.enable = true;
-    i3.enable = true;
-    i3status.enable = true;
-    neovim.enable = true;
-    rofi.enable = true;
-    x11.enable = true;
-    zsh.enable = true;
-  };
+  imports = [ ../home ];
 
   # @TODO: This is not working
   services = {
